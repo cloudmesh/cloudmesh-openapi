d8512f110bc39e736863216e598b1d3e0479c607
Gregor von Laszewski <laszewski@gmail.com>
2019-05-06 18:23:35 -0400

    add cloudmesh dependencies

--- setup.py
+++ setup.py
@@ -31,6 +31,9 @@ def readfile(filename):
 requiers = """
 ruamel.yaml
 connexion[swagger-ui]
+cloudmesh-common
+cloudmesh-cmd5
+cloudmesh-sys
 """.split("\n")
 
 # dependency_links = ['http://github.com/nicolaiarocci/eve.git@develop']
