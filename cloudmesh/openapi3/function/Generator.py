ba89e67c22f70a5c83cf80d49fbb3226b6c321dc
bkegerreis <bjkegerr@iu.edu>
2020-02-29 22:51:26 -0500

    added portable function to parse types

--- cloudmesh/openapi3/function/Generator.py
+++ cloudmesh/openapi3/function/Generator.py
@@ -24,8 +24,22 @@ paths:
               schema: 
                 type: {return_type}
       parameters:
-         {parameters} 
+        {parameters} 
 """
+
+    def parse_type(self,_type):
+        parser = {
+                int: 'integer',
+                bool: 'boolean',
+                float: 'float',
+                str: 'str',
+                list: textwrap.dedent('array\nitems: {}')
+                }
+        try:
+            t=parser[_type]
+        except KeyError:
+            t='object'
+        return t
 
     def generate_parameter(self, name, _type, description):
         """ function to generate parameters YAMAL contents"""
@@ -41,27 +55,20 @@ paths:
         """ Function to loop all the parameters of given function and generate specification"""
         spec = str()
         for parameter, _type in functionName.__annotations__.items():
-            if parameter == "return":
+            if parameter == 'return':
                 break
-            if _type == int:
-                _type = 'integer'
-            elif _type == bool:
-                _type = 'boolean'
-            elif _type == float:
-                _type = 'float'
             else:
-                _type = 'unkown'
-            spec = spec + self.generate_parameter(parameter, _type, "not yet available, you can read it from docstring")
+                spec = spec + self.generate_parameter(parameter, parse_type(_type), "not yet available, you can read it from docstring")
         return spec
-
+    
     def generate_openapi(self, f, write=True):
         """ function to generate open API of python function."""
         description = f.__doc__.strip().split("\n")[0]
         version = "1.0"  # hard coded for now
         title = f.__name__
-        return_type = str()
+        return_type = parse_type(f.__annotations__['return'])
         parameters = self.populateParameters(f)
-        parameters = textwrap.indent(parameters, ' ' * 9)
+        parameters = textwrap.indent(parameters, ' ' * 8)
         spec = self.openAPITemplate.format(
             title=title,
             name=f.__name__,
