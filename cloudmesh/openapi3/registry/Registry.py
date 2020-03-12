b104f3c807a58d2c940eab93d145219ddb8bdde4
Gregor von Laszewski <laszewski@gmail.com>
2020-03-12 14:51:38 -0400

    add registry template

--- cloudmesh/openapi3/registry/Registry.py
+++ cloudmesh/openapi3/registry/Registry.py
@@ -1,4 +1,5 @@
 from cloudmesh.mongo.DataBaseDecorator import DatabaseUpdate
+from cloudmesh.common.Shell import Shell
 
 class Registry:
 
@@ -6,7 +7,7 @@ class Registry:
         self.url = url
 
     @DatabaseUpdate()
-    def generate_entry(self, name=None):
+    def add(self, name=None, url=None):
         entry = {
             "cm": {
                 "cloud": "local",
@@ -14,7 +15,52 @@ class Registry:
                 "name": name,
                 "dirver": None
             },
-            "url": self.url,
+            "url": url,
             "name": name
         }
         return entry
+
+    def add_form_file(self, filename):
+        """
+
+        :param filename:
+        :return:
+        """
+        raise NotImplementedError
+
+    def delete(self, name=None):
+        """
+
+        :param name:
+        :return:
+        """
+        raise NotImplementedError
+
+    def list(self, name=None):
+        """
+
+        :param name:  if none all
+        :return:
+        """
+        raise NotImplementedError
+
+    def start(self):
+        """
+        start the registry
+
+        possibly not needed as we have cms start
+
+        :return:
+        """
+        r = Shell.cms("start")
+
+    def stop(self):
+        """
+        stop the registry
+
+        possibly not needed as we have cms start
+        this will not just sto the registry but mongo
+
+        :return:
+        """
+        r = Shell.cms("stop")
