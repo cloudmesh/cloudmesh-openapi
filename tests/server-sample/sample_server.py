69532aac404449d266afc7d9569d83cca41765a7
Gregor von Laszewski <laszewski@gmail.com>
2020-03-12 21:21:13 -0400

    add sample

---
+++ tests/server-sample/sample_server.py
@@ -0,0 +1,10 @@
+import connexion
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the yaml file to configure the endpoints
+app.add_api('sample.yaml')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=8080,debug=False,server=None)
