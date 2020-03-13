1b58693fea0ae1b89011942f42f0ee65029bb8a3
Gregor von Laszewski <laszewski@gmail.com>
2020-03-13 09:51:28 -0400

    add cms example

--- tests/server-cms/cms.py
+++ tests/server-cms/cms.py
@@ -1,9 +1,11 @@
 from cloudmesh.compute.vm.Provider import Provider
 from flask import jsonify
+from pprint import pprint
 
 #def list(cloud):
-def list():
+def vms():
     cloud = "chameleon"
     provider = Provider(name=cloud)
     vms = provider.list()
+    pprint(vms)
     return jsonify(vms)
