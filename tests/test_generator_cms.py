8bf7fd6515a93b8b7e37f3883e7ebc533b0030f5
Gregor von Laszewski <laszewski@gmail.com>
2020-03-19 18:44:41 -0400

    add a quicu test for cpu

---
+++ tests/test_generator_cms.py
@@ -0,0 +1,43 @@
+###############################################################
+# pytest -v --capture=no tests/test_generator_cms.py
+# pytest -v  tests/test_generator_cms.py
+# pytest -v --capture=no  tests/test_generator_cms..py::Test_generator_cms::<METHODNAME>
+###############################################################
+import pytest
+from cloudmesh.common.Shell import Shell
+from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.util import HEADING
+from cloudmesh.common.Benchmark import Benchmark
+
+Benchmark.debug()
+
+cloud = "local"
+
+
+@pytest.mark.incremental
+class TestGeneratorCms:
+
+    def test_start(self):
+        HEADING()
+
+        Benchmark.Start()
+        result = Shell.execute("cms openapi3 server start ./tests/cpu.yaml", shell=True)
+        Benchmark.Stop()
+        VERBOSE(result)
+
+        #assert False # find test
+
+    def test_stop(self):
+        HEADING()
+
+        Benchmark.Start()
+        result = Shell.execute("cms openapi3 server stop cpu", shell=True)
+        Benchmark.Stop()
+        VERBOSE(result)
+
+        #assert False # find test
+
+
+    def test_benchmark(self):
+        HEADING()
+        Benchmark.print(csv=True, tag=cloud)
