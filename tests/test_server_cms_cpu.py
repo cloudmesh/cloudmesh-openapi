e56b21f984650b1699b3dddcaa8f64d66dd860c9
prateekazam@gmail.com <prateekazam@gmail.com>
2020-03-19 19:19:31 -0400

    Added test case template for cpu

--- tests/test_server_cms_cpu.py
+++ tests/test_server_cms_cpu.py
@@ -38,6 +38,45 @@ class TestServerCms:
 
         #assert False # find test
 
+    def test_ui(self):
+        HEADING()
+        url="http://127.0.0.1:8080/cloudmesh/ui/"
+        Benchmark.Start()
+        # result = Shell.execute(f"cms openapi3 server stop {name}", shell=True)
+        result = requests.get('https://api.github.com/user')
+        assert result.status_code == 200
+        # >> > r.headers['content-type']
+        # 'application/json; charset=utf8'
+        # >> > r.encoding
+        # 'utf-8'
+        # >> > r.text
+        # u'{"type":"User"...'
+        # >> > r.json()
+        # {u'private_gists': 419, u'total_private_repos': 77, ...}
+        Benchmark.Stop()
+        VERBOSE(result)
+
+        # assert False # find test
+
+    def test_cpu(self):
+        HEADING()
+        url = "http://127.0.0.1:8080/cloudmesh/cpu"
+        Benchmark.Start()
+        # result = Shell.execute(f"cms openapi3 server stop {name}", shell=True)
+        result = requests.get('https://api.github.com/user')
+        assert result.status_code == 200
+        # >> > r.headers['content-type']
+        # 'application/json; charset=utf8'
+        # >> > r.encoding
+        # 'utf-8'
+        # >> > r.text
+        # u'{"type":"User"...'
+        # >> > r.json()
+        # {u'private_gists': 419, u'total_private_repos': 77, ...}
+        Benchmark.Stop()
+        VERBOSE(result)
+
+        # assert False # find test
 
     def test_benchmark(self):
         HEADING()
