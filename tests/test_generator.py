7f3ab8cbb4751cb71b0917c651bda5fc7aa6e63b
Andrew Goldfarb <andrewgoldfarb@Andrews-MacBook-Pro.local>
2020-03-21 18:04:50 -0400

    added real_test function to tests for generator to test server start fucntionality

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -5,23 +5,27 @@
 ###############################################################
 import pytest
 import yaml as yaml
+import time
 import sys
+
 
 from cloudmesh.common.StopWatch import StopWatch
 from cloudmesh.common.util import HEADING
 from cloudmesh.common.util import path_expand
 from cloudmesh.common.Benchmark import Benchmark
 from pprint import pprint
+from cloudmesh.common.Shell import Shell
+from cloudmesh.mongo.CmDatabase import CmDatabase
+
 from cloudmesh.mongo.DataBaseDecorator import DatabaseUpdate
 from cloudmesh.openapi3.registry.Registry import Registry
 # sys.path.append("cloudmesh/openapi3/function")
 #
 
-
 #
 # get the spec for the tests
 #
-with open("tests/sampleFunction.yaml", "r") as stream:
+with open("./generator/sampleFunction.yaml", "r") as stream:
     try:
         spec = yaml.safe_load(stream)
     except yaml.YAMLError as e:
@@ -79,21 +83,34 @@ class TestGenerator:
 
         Benchmark.Stop()
 
-
-    def real_test(self):
+    def test_real(self):
         """
-        Go to directory with python script and yaml:
-        cd ~/cm/cloudmesh-openapi/tests/generator
-        2. Start:
-        cms openapi3 server start sampleFunction.yaml samplefunction --directory=~/cm/cloudmesh-openapi/tests/generator/
-        3. Test out endpoint:
-        http://127.0.0.1:8080/cloudmesh/generator?x=3&y=2.5
-        4. Stop:
-        cms openapi3 server stop samplefunction
-
-        :return:
+        function to test if the server is started and available to return
+        a successful http code
         """
-        assert False
+        HEADING()
+        Benchmark.Start()
+
+        Shell.run("cd ~/cm/cloudmesh-openapi/tests/")
+        test_loc = Shell.run("pwd")
+        test_loc = test_loc.strip() + "/"
+
+        assert test_loc == "/Users/andrewgoldfarb/e516-spring/cm/cloudmesh-openapi/tests/"
+
+        server_output = Shell.cms("openapi3 server start ./server-cpu/cpu.yaml")
+        assert server_output.__contains__("starting server")
+
+        time.sleep(2)
+        baseurl = "http://127.0.0.1:8080/cloudmesh"
+        response = Shell.run("curl " + baseurl + "/cpu")
+        assert response.__contains__("200")
+
+        Shell.cms("openapi3 server stop cpu")
+        response = Shell.run("curl " + baseurl + "/cpu")
+        fail_message="Failed to connect to 127.0.0.1 port 80: Connection refuse"
+        assert response.__contains__(fail_message)
+
+        Benchmark.Stop()
 
     def test_benchmark(self):
         HEADING()
