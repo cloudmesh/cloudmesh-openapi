577e2154e41ffabaa580634db8f950c4dd552343
Gregor von Laszewski <laszewski@gmail.com>
2020-04-06 21:27:13 -0400

    add the docstring example

---
+++ example/doc.py
@@ -0,0 +1,11 @@
+def sample(a: int, b: float, c: str) -> str:
+    """
+    This is a sample
+
+    :param a: this is a
+              with two lines
+    :param b: this is b
+    :param c: this is c
+    :return: this is a string conatining that
+    """
+    pass
