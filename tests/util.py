310cdb97e9a01598d3c311a37ffa2bd84063616f
Gregor von Laszewski <laszewski@gmail.com>
2020-04-08 17:22:56 -0400

    add the util

--- tests/util.py
+++ tests/util.py
@@ -1,8 +1,8 @@
 import oyaml as yaml
-
+from cloudmesh.common.util import path_expand
 
 def readyaml(name):
-    with open(name, "r") as stream:
+    with open(path_expand(name), "r") as stream:
         try:
             spec = yaml.safe_load(stream)
         except yaml.YAMLError as e:
