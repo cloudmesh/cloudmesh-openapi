0adf68a59dea612379581e93d1d58061682ff715
Gregor von Laszewski <laszewski@gmail.com>
2020-04-08 19:10:03 -0400

    path printer that seems to make more sense

--- cloudmesh/openapi/function/executor.py
+++ cloudmesh/openapi/function/executor.py
@@ -1,12 +1,34 @@
 import os
 import textwrap
-from pathlib import Path
 
 from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
 
 
 class Parameter:
+    """
+    To generat a useful output for the variables. Example:
+
+        from cloudmesh.openapi.function.executor import Parameter
+        p = Parameter(arguments)
+        p.Print()
+
+    Returns
+
+        Cloudmesh OpenAPI Generator:
+
+          File Locations:
+            - Currdir:    .
+            - Directory:  ./tests/generator-calculator
+            - Filename:   ./tests/generator-calculator/calculator.py
+            - YAML:       ./tests/generator-calculator/calculator.yaml
+
+          Yaml File Related:
+            - Function:   calculator
+            - Servereurl: http://sample.org/cloudmesh/
+            - Module:     None
+
+    """
 
     def __init__(self, arguments):
         self.arguments = arguments
@@ -21,17 +43,22 @@ class Parameter:
 
     def get(self, arguments):
 
+        self.cwd = path_expand(os.path.curdir)
+
         filename = arguments.filename
 
         if filename is None:
             Console.error(f"--filename={filename}")
         self.filename = path_expand(filename)
         if not os.path.isfile(filename):
-            Console.error(f"--filename={filename} does not exist")
-        self.yamlfile = self.yamlfile or self.filename.rsplit(".")[1] + ".yaml"
-        self.directory = arguments.dir or Path(filename).stem
+            Console.error(f"--filename={self.filename} does not exist")
 
-        self.function = arguments.FUNCTION or os.path.basename(filename).stem
+        self.yamlfile = self.yamlfile or self.filename.rsplit(".py")[
+            0] + ".yaml"
+        self.directory = arguments.dir or os.path.dirname(self.filename)
+
+        self.function = arguments.FUNCTION or os.path.basename(
+            self.filename).stem
         self.serverurl = arguments.serverurl or "http://sample.org/cloudmesh/"
 
     def Print(self):
@@ -40,9 +67,10 @@ class Parameter:
              Cloudmesh OpenAPI Generator:
 
                File Locations:
-                 - Directory:  {self.directory}
-                 - Filename:   {self.filename}
-                 - YAML:       {self.yamlfile}
+                 - Currdir:    .
+                 - Directory:  {self.directory.replace(self.cwd, ".")}
+                 - Filename:   {self.filename.replace(self.cwd, ".")}
+                 - YAML:       {self.yamlfile.replace(self.cwd, ".")}
 
                Yaml File Related:
                  - Function:   {self.function}
