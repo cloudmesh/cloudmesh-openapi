709420050bec6dde7cba8fabda8a2086a3b3d0d0
Gregor von Laszewski <laszewski@gmail.com>
2020-04-07 22:19:58 -0400

    add image analysis

---
+++ example/image-analyis/imape.py
@@ -0,0 +1,14 @@
+def detect_handwriting(service, image):
+    pass
+
+
+def detect_text(service, image):
+    pass
+
+
+def detect(service, kind, image):
+    pass
+
+
+service in aws, google
+kind in handwriting, text
