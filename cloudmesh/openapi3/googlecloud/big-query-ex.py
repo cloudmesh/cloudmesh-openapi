9992cbfc0dd67e6f98b89f04c56bac8813246305
Andrew Goldfarb <andrewgoldfarb@Andrews-MacBook-Pro.local>
2020-04-11 13:09:33 -0400

    added big query examples and yaml function for api

--- cloudmesh/openapi3/googlecloud/big-query-ex.py
+++ cloudmesh/openapi3/googlecloud/big-query-ex.py
@@ -6,23 +6,25 @@ from google.cloud import bigquery
 #     self.dataset = dataset
 #     self.table = table
 #     self.client = client
+class Google:
 
+    def pushFileBQ(dataset):
 
-client = bigquery.Client()
-filename = '/Users/andrewgoldfarb/Desktop/test-data/data/flavors.csv'
-dataset_id = "My_dataset"
-table_id = "my_table"
+        client = bigquery.Client()
+        filename = '/Users/andrewgoldfarb/Desktop/test-data/data/flavors.csv'
+        dataset_id = dataset
+        table_id = "my_table"
+        
+        dataset_ref = client.dataset(dataset_id)
+        table_ref = dataset_ref.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = bigquery.SourceFormat.CSV
+        job_config.skip_leading_rows = 1
+        job_config.autodetect = True
 
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.CSV
-job_config.skip_leading_rows = 1
-job_config.autodetect = True
+        with open(filename, "rb") as source_file:
+            job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
 
-with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+        job.result()  # Waits for table load to complete.
 
-job.result()  # Waits for table load to complete.
-
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+        print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
