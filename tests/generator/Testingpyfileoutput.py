59d356de10c3afa964c58bf925ddb9147a46170a
jkandima <jkandimaedu@gmail.com>
2020-04-12 19:28:46 -0500

    Updated test for Logistic Regression

--- tests/generator/Testingpyfileoutput.py
+++ tests/generator/Testingpyfileoutput.py
@@ -1,13 +1,17 @@
 from tests.generator import LogisticRegression
+from sklearn.datasets import load_iris
 import numpy as np
-
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-y = np.dot(X, np.array([1, 2])) + 3
-estimator = LogisticRegression.set_params(penalty='l1',solver='saga')
+X, y = load_iris(return_X_y=True)
+#X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+#y = np.dot(X, np.array([1, 2])) + 3
+estimator = LogisticRegression.set_params(random_state =0)
+print(estimator.get_params)
 fit = estimator.fit(X,y,None)
+print(fit.predict(X[:2, :]))
+print(fit.predict_proba(X[:2, :]))
 score = fit.score(X,y)
-
-print(score,fit)
+print(score)
+print(fit)
 
 
 
