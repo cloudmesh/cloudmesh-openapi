29d0ee63fdf5d0ff3f2b2ca8c50bd62a02c064e0
Adam Chai <ajchai@iu.edu>
2020-12-01 20:58:29 -0800

    fixed bash command in loop

--- tests/repeat.py
+++ tests/repeat.py
@@ -3,7 +3,7 @@ count = 1
 for x in range(0,30):
 	print(count, "iteration")
 	count += 1
-    bashCmd = "pytest -v  -s ./tests/test_030_generator_eigenfaces_svm.py"
+    bashCmd = "pytest -v  -s /home/pi/cm/cloudmesh-openapi/tests/test_030_generator_eigenfaces_svm.py"
     output = Shell.run(bashCmd)
     outF = open("myOutFile.txt", "a")
     outF.write(str(output))
