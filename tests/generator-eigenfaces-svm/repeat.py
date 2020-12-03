1ed6fd453938a0391b70aa07baefcb5f31b63f01
Adam <ajchai@iu.edju>
2020-12-03 23:32:42 +0000

    fixed indentation

--- tests/generator-eigenfaces-svm/repeat.py
+++ tests/generator-eigenfaces-svm/repeat.py
@@ -1,8 +1,8 @@
 from cloudmesh.common.Shell import Shell
 count = 1
 for x in range(0,30):
-	print(count, "iteration")
-	count += 1
+    print(count, "iteration")
+    count += 1
     bashCmd = "pytest -v  -s /home/pi/cm/cloudmesh-openapi/tests/test_030_generator_eigenfaces_svm.py"
     output = Shell.run(bashCmd)
     outF = open("myOutFile4.txt", "a")
